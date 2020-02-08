8d65850163d416bc5fa199092288058819fe87b8
Brian <brianvu7@gmail.com>
2020-02-08 02:52:24 -0600

    Messed around with adding more shapes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var divCounter = 0;
+
 document.querySelector("#make-floor-plan").addEventListener("click", function(event){
     event.preventDefault();
 
@@ -13,80 +15,85 @@ document.querySelector("#make-rect").addEventListener("click", function(event){
 
     let dragDiv = document.createElement("div");
     dragDiv.setAttribute("id", "draggable");
-    dragDiv.setAttribute("style", "height:" + (parseInt(document.querySelector("#rect-height").value) + 10) + "px; width:" + (parseInt(document.querySelector("#rect-width").value) + 10) + "px");
+    dragDiv.setAttribute("style", "height:" + (parseInt(document.querySelector("#rect-height").value) + 10) + "px; width:" + (parseInt(document.querySelector("#rect-width").value) + 10) + "px; position:absolute; cursor:move");
     document.querySelector("body").appendChild(dragDiv);
 
     console.log(parseInt(document.querySelector("#rect-height").value) + 10);
     console.log(parseInt(document.querySelector("#rect-width").value) + 10);
     console.log(dragDiv.getAttribute("style"));
     console.log(dragDiv.getAttribute("id"));
+    console.log(document.querySelector(("#draggable-" + divCounter)))
 
+    let newIllElement = "canvas-child-" + divCounter;
     let newCanvas = document.createElement("canvas");
-    newCanvas.classList.add("canvas-child");
+    newCanvas.classList.add(newIllElement);
     newCanvas.setAttribute("width", document.querySelector("#rect-width").value);
     newCanvas.setAttribute("height", document.querySelector("#rect-height").value);
     document.querySelector("#draggable").appendChild(newCanvas);
-
+    
+    console.log(newCanvas.getAttribute("class"));
+    
     let userShape = new Zdog.Illustration({
-        element: ".canvas-child",
+        element: "." + newIllElement,
         zoom: 4,
         dragRotate: true,
     });
-
+    
     let rect = new Zdog.Rect({
-      addTo: userShape,
-      width: document.querySelector("#rect-width").value,
-      height: document.querySelector("#rect-height").value,
-      translate: {z: 10},
-      stroke: 1,
-      color: '#E62',
-      fill: true
+        addTo: userShape,
+        width: document.querySelector("#rect-width").value,
+        height: document.querySelector("#rect-height").value,
+        translate: {z: 10},
+        stroke: 1,
+        color: '#E62',
+        fill: true
     });
+
+    //divCounter++;
 
     userShape.updateRenderGraph();
     dragElement(document.querySelector("#draggable"));
 })
 
 //Code taken from https://www.w3schools.com/howto/howto_js_draggable.asp
-// Make the DIV element draggable:
-
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.querySelector("draggable")) {
-    // if present, the header is where you move the DIV from:
-    document.querySelector("draggable").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+   if (document.querySelector("#draggable")) {
+     // if present, the header is where you move the DIV from:
+     document.querySelector("#draggable").onmousedown = dragMouseDown;
+   }
+   else {
+     // otherwise, move the DIV from anywhere inside the DIV:
+     elmnt.onmousedown = dragMouseDown;
+   }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+   function dragMouseDown(e) {
+     e = e || window.event;
+     e.preventDefault();
+     // get the mouse cursor position at startup:
+     pos3 = e.clientX;
+     pos4 = e.clientY;
+     document.onmouseup = closeDragElement;
+     // call a function whenever the cursor moves:
+     document.onmousemove = elementDrag;
+    }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+   function elementDrag(e) {
+     e = e || window.event;
+     e.preventDefault();
+     // calculate the new cursor position:
+     pos1 = pos3 - e.clientX;
+     pos2 = pos4 - e.clientY;
+     pos3 = e.clientX;
+     pos4 = e.clientY;
+     // set the element's new position:
+     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+   }
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+   function closeDragElement() {
+     // stop moving when mouse button is released:
+     document.onmouseup = null;
+     document.onmousemove = null;
+   }
 }
