5199e551a4cd32edae6fa53aaf85776ae5ef7948
Brian <brianvu7@gmail.com>
2020-02-10 02:24:50 -0600

    Added a list of items, made multiple shapes working, added labels

--- script.js
+++ script.js
@@ -11,27 +11,153 @@ document.querySelector("#make-floor-plan").addEventListener("click", function(ev
 })
 
 document.querySelector("#make-shape").addEventListener("click", function(event){
-    event.preventDefault();
-
-    var shapeChosen = document.querySelector("#shape-options");
-    var shape = shapeChosen.value;
-    var userHeight = parseInt(document.querySelector("#height").value);
-    var userWidth = parseInt(document.querySelector("#width").value);
-    var userColor = document.querySelector("#color").value;
-
-    createShape(shape, userHeight, userWidth, userColor);
+  event.preventDefault();
+  var shape = document.querySelector("#shape-options").value;
+  var userHeight = parseInt(document.querySelector("#height").value);
+  var userWidth = parseInt(document.querySelector("#width").value);
+  var userColor = document.querySelector("#color").value;
+  var userLabel = document.querySelector("#label").value;
+  createShape(shape, userHeight, userWidth, userColor, userLabel);
 })
 
-function createShape(shape, userHeight, userWidth, userColor){
+document.querySelector("#shape-options").addEventListener("change", function(event){
+  event.preventDefault();
+  var shape = document.querySelector("#shape-options").value;
 
+  if(shape === "rectangle" || shape === "ellipse"){
+    createForm(shape);
+      
+    document.querySelector("#create").addEventListener("click", function(event){
+      event.preventDefault();
+      var userHeight = parseInt(document.querySelector("#height").value);
+      var userWidth = parseInt(document.querySelector("#width").value);
+      var userColor = document.querySelector("#color").value;
+      var userLabel = document.querySelector("#label").value;
+      createShape(shape, userHeight, userWidth, userColor, userLabel);
+    });
+  }
+  else if(shape === "triangle"){
+    var numSides = 3;
+
+    createForm(shape)
+
+    document.querySelector("#create").addEventListener("click", function(event){
+      event.preventDefault();
+      var userRadius = parseInt(document.querySelector("#radius").value);
+      var userColor = document.querySelector("#color").value;
+      var userLabel = document.querySelector("#label").value;
+      createPolygon(numSides, userRadius, userColor, userLabel);
+    });
+  }
+  else if(shape === "pentagon"){
+    var numSides = 5;
+
+    createForm(shape)
+
+    document.querySelector("#create").addEventListener("click", function(event){
+      event.preventDefault();
+      var userRadius = parseInt(document.querySelector("#radius").value);
+      var userColor = document.querySelector("#color").value;
+      var userLabel = document.querySelector("#label").value;
+      createPolygon(numSides, userRadius, userColor, userLabel);
+    });
+  }
+  else if(shape === "hexagon"){
+    var numSides = 6;
+
+    createForm(shape)
+
+    document.querySelector("#create").addEventListener("click", function(event){
+      event.preventDefault();
+      var userRadius = parseInt(document.querySelector("#radius").value);
+      var userColor = document.querySelector("#color").value;
+      var userLabel = document.querySelector("#label").value;
+      createPolygon(numSides, userRadius, userColor, userLabel);
+    });
+  }
+})
+
+function createForm(currentShape){
+  var input = document.querySelector("#dimension-input");
+  while(input.firstChild){
+    input.removeChild(input.firstChild);
+  }
+
+  if(currentShape === "rectangle" || currentShape === "ellipse"){
+
+    var height = document.createElement("input");
+    height.setAttribute("type", "text");
+    height.setAttribute("id", "height");
+    height.setAttribute("placeholder", "Height");
+    input.appendChild(height);
+
+    var width = document.createElement("input");
+    width.setAttribute("type", "text");
+    width.setAttribute("id", "width");
+    width.setAttribute("placeholder", "Width");
+    input.appendChild(width);
+
+    var color = document.createElement("input");
+    color.setAttribute("type", "text");
+    color.setAttribute("id", "color");
+    color.setAttribute("placeholder", "Color");
+    input.appendChild(color);
+
+    var label = document.createElement("input");
+    label.setAttribute("type", "text");
+    label.setAttribute("id", "label");
+    label.setAttribute("placeholder", "Label");
+    input.appendChild(label);
+
+    var create = document.createElement("input");
+    create.setAttribute("type", "submit");
+    create.setAttribute("id", "create");
+    create.setAttribute("value", "Create Object");
+    input.appendChild(create);
+  }
+  else if(currentShape === "triangle" || currentShape === "pentagon" || currentShape === "hexagon"){
+
+    var radius = document.createElement("input");
+    radius.setAttribute("type", "text");
+    radius.setAttribute("id", "radius");
+    radius.setAttribute("placeholder", "Radius");
+    input.appendChild(radius);
+
+    var color = document.createElement("input");
+    color.setAttribute("type", "text");
+    color.setAttribute("id", "color");
+    color.setAttribute("placeholder", "Color");
+    input.appendChild(color);
+
+    var label = document.createElement("input");
+    label.setAttribute("type", "text");
+    label.setAttribute("id", "label");
+    label.setAttribute("placeholder", "Label");
+    input.appendChild(label);
+
+    var create = document.createElement("input");
+    create.setAttribute("type", "submit");
+    create.setAttribute("id", "create");
+    create.setAttribute("value", "Create Object");
+    input.appendChild(create);
+  }
+}
+
+function createShape(shape, userHeight, userWidth, userColor, userLabel){
   switch(shape){
 
     case "rectangle":
 
       var dragDiv = document.createElement("div");
+      dragDiv.innerHTML = userLabel;
       dragDiv.setAttribute("class", "draggable");
       dragDiv.setAttribute("id", "draggable-" + divCounter)
-      dragDiv.setAttribute("style", "height:" + (userHeight) + "px; width:" + (userWidth + 30) + "px");
+      dragDiv.setAttribute("style", "height:" + (userHeight + 20) + "px; width:" + (userWidth + 40) + "px");
+      dragDiv.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+        $(this).remove();
+        return false;
+      })
       document.querySelector("#shape-list").appendChild(dragDiv);
 
       var newIllElement = "canvas-child-" + divCounter;
@@ -40,11 +166,11 @@ function createShape(shape, userHeight, userWidth, userColor){
       newCanvas.setAttribute("width", (userWidth + 20));
       newCanvas.setAttribute("height", (userHeight + 20));
       document.querySelector("#draggable-" + divCounter).appendChild(newCanvas);
+      divCounter++;
 
       var userShape = new Zdog.Illustration({
         element: "." + newIllElement,
         zoom: 1,
-        dragRotate: true,
       });
 
       new Zdog.Rect({
@@ -56,8 +182,42 @@ function createShape(shape, userHeight, userWidth, userColor){
         fill: true
       });
 
+      //Making the shape again and adding it to the list of items
+      var listCanvasElement = "canvas-child-" + divCounter;
+      var listDiv = document.createElement("div");
+      listDiv.setAttribute("style", "height:" + (userHeight + 20) + "px; width:" + (userWidth + 40) + "px; text-align:center");
+      listDiv.innerHTML = userLabel;
+      listDiv.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+        $(this).remove();
+        return false;
+      })
+      document.querySelector("#item-list").appendChild(listDiv);
+      var newListCanvas = document.createElement("canvas");
+      newListCanvas.classList.add(listCanvasElement);
+      newListCanvas.setAttribute("width", (userWidth + 20));
+      newListCanvas.setAttribute("height", (userHeight));
+      listDiv.appendChild(newListCanvas);
+
+      var listIll = new Zdog.Illustration({
+        element: "." + listCanvasElement,
+        zoom: 1,
+        dragRotate: true
+      });
+
+      new Zdog.Rect({
+        addTo: listIll,
+        width: userWidth,
+        height: userHeight,
+        translate: {z: 10},
+        color: userColor,
+        fill: true
+      });
+
+      listIll.updateRenderGraph();
+      
       function animate() {
-        userShape.updateRenderGraph();
+        listIll.updateRenderGraph();
         requestAnimationFrame( animate );
       }
       animate();
@@ -66,9 +226,15 @@ function createShape(shape, userHeight, userWidth, userColor){
     case "ellipse":
 
       var dragDiv = document.createElement("div");
+      dragDiv.innerHTML = userLabel;
       dragDiv.setAttribute("class", "draggable");
       dragDiv.setAttribute("id", "draggable-" + divCounter)
-      dragDiv.setAttribute("style", "height:" + (userHeight) + "px; width:" + (userWidth + 15) + "px");
+      dragDiv.setAttribute("style", "height:" + (userHeight + 5) + "px; width:" + (userWidth + 25) + "px");
+      dragDiv.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+        $(this).remove();
+        return false;
+      })
       document.querySelector("#shape-list").appendChild(dragDiv);
 
       var newIllElement = "canvas-child-" + divCounter;
@@ -77,11 +243,11 @@ function createShape(shape, userHeight, userWidth, userColor){
       newCanvas.setAttribute("width", parseInt(userWidth + 5));
       newCanvas.setAttribute("height", parseInt(userHeight + 5));
       document.querySelector("#draggable-" + divCounter).appendChild(newCanvas);
+      divCounter++;
 
       var userShape = new Zdog.Illustration({
         element: "." + newIllElement,
         zoom: 1,
-        dragRotate: true,
       });
 
       new Zdog.Ellipse({
@@ -92,6 +258,40 @@ function createShape(shape, userHeight, userWidth, userColor){
         color: userColor,
         fill: true
       });
+
+      //Making the shape again and adding it to the list of items
+      var listCanvasElement = "canvas-child-" + divCounter;
+      var listDiv = document.createElement("div");
+      listDiv.setAttribute("style", "height:" + (userHeight + 25) + "px; width:" + (userWidth + 25) + "px; text-align:center");
+      listDiv.innerHTML = userLabel;
+      listDiv.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+        $(this).remove();
+        return false;
+      })
+      document.querySelector("#item-list").appendChild(listDiv);
+      var newListCanvas = document.createElement("canvas");
+      newListCanvas.classList.add(listCanvasElement);
+      newListCanvas.setAttribute("width", (userWidth + 5));
+      newListCanvas.setAttribute("height", (userHeight + 5));
+      listDiv.appendChild(newListCanvas);
+
+      var listIll = new Zdog.Illustration({
+        element: "." + listCanvasElement,
+        zoom: 1,
+        dragRotate: true
+      });
+
+      new Zdog.Ellipse({
+        addTo: listIll,
+        width: userWidth,
+        height: userHeight,
+        translate: {z: 10},
+        color: userColor,
+        fill: true
+      });
+
+      listIll.updateRenderGraph();
 
       function animate() {
         userShape.updateRenderGraph();
@@ -106,17 +306,98 @@ function createShape(shape, userHeight, userWidth, userColor){
   userShape.updateRenderGraph();
 
   var dragClass = document.getElementsByClassName("draggable");
-
   for(var i = 0; i < dragClass.length; i++){
     dragElement(dragClass[i]);
   }
-
-  animate(userShape);
 }
 
-function animate(userShape){
+function createPolygon(numSides, userRadius, userColor, userLabel){
+  var dragDiv = document.createElement("div");
+  dragDiv.innerHTML = userLabel;
+  dragDiv.setAttribute("class", "draggable");
+  dragDiv.setAttribute("id", "draggable-" + divCounter)
+  dragDiv.setAttribute("style", "height:" + (userRadius + 70) + "px; width:" + (userRadius + 70) + "px");
+  dragDiv.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    $(this).remove();
+    return false;
+  })
+  document.querySelector("#shape-list").appendChild(dragDiv);
+
+  var newIllElement = "canvas-child-" + divCounter;
+  var newCanvas = document.createElement("canvas");
+  newCanvas.classList.add(newIllElement);
+  newCanvas.setAttribute("width", parseInt(userRadius + 50));
+  newCanvas.setAttribute("height", parseInt(userRadius + 50));
+  document.querySelector("#draggable-" + divCounter).appendChild(newCanvas);
+
+  var userShape = new Zdog.Illustration({
+    element: "." + newIllElement,
+    zoom: 1,
+  });
+
+  new Zdog.Polygon({
+    addTo: userShape,
+    radius: userRadius,
+    sides: numSides,
+    translate: {z: 10},
+    color: userColor,
+    fill: true
+  });
+
+  //Making the shape again and adding it to the list of items
+  var listCanvasElement = "canvas-child-" + divCounter;
+  var listDiv = document.createElement("div");
+  listDiv.setAttribute("style", "height:" + (userRadius + 70) + "px; width:" + (userRadius + 70) + "px; text-align:center");
+  listDiv.innerHTML = userLabel;
+  listDiv.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    $(this).remove();
+    return false;
+  })
+  document.querySelector("#item-list").appendChild(listDiv);
+  var newListCanvas = document.createElement("canvas");
+  newListCanvas.classList.add(listCanvasElement);
+  newListCanvas.setAttribute("width", (userRadius + 50));
+  newListCanvas.setAttribute("height", (userRadius + 50));
+  listDiv.appendChild(newListCanvas);
+
+  var listIll = new Zdog.Illustration({
+    element: "." + listCanvasElement,
+    zoom: 1,
+    dragRotate: true
+  });
+
+  new Zdog.Polygon({
+    addTo: listIll,
+    radius: userRadius,
+    sides: numSides,
+    translate: {z: 10},
+    color: userColor,
+    fill: true
+  });
+
+  listIll.updateRenderGraph();
+
+  function animate() {
+    userShape.updateRenderGraph();
+    requestAnimationFrame( animate );
+  }
+  animate();
+
+  divCounter++;
+
   userShape.updateRenderGraph();
-  requestAnimationFrame( animate );
+
+  var dragClass = document.getElementsByClassName("draggable");
+  for(var i = 0; i < dragClass.length; i++){
+    dragElement(dragClass[i]);
+  }
+}
+
+function addItem(userItem){
+  var list = document.querySelector("#item-list");
+  var item;
 }
 
 //Code taken from https://www.w3schools.com/howto/howto_js_draggable.asp
